e2bb44f88942e9d7a6a74f240fa1a59a14ba4b40
Drew Moseley <drew.moseley@northern.tech>
2017-08-09 14:57:01 -0400

    Initial ispell support
    Signed-off-by: Drew Moseley <drew.moseley@northern.tech>

---
+++ spell-check.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# Run the command line ispell utility on all markdown files.
+# Note this uses a custom dictionary located in .ispell_english.
+# File checksum, crypto hases and Bitbake variables are difficult to account
+# for so this dictionary is rather large and will need to be added to over time.
+
+ispell $(find . -name '*.md')
