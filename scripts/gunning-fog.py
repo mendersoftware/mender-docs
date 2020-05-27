c6ede68e7a570489ac3e6488d82df5b3acf9bf36
Ole Petter <ole.orhagen@northern.tech>
2020-05-27 09:07:04 +0200

    Add: Documentation style enforcement
    Changelog: None
    
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

---
+++ scripts/gunning-fog.py
@@ -0,0 +1,31 @@
+from readability import Readability
+from readability.exceptions import ReadabilityException
+import sys
+
+if len(sys.argv) <= 1:
+    print("No files given")
+    sys.exit(1)
+
+exit_code = 0
+
+for file in sys.argv[1:]:
+    with open(file) as f:
+        text = f.read()
+        r = Readability(text)
+        try:
+            gf = r.gunning_fog()
+        except ReadabilityException as e:
+            print("{}: {}".format(file, e))
+            sys.exit(0)
+
+        if not 9 <= int(gf.score) <= 12:
+            print(
+                "Gunning-Fog index for {} is {}, and not in the range [9, 12]".format(
+                    file, gf.score
+                )
+            )
+            exit_code = 1
+        else:
+            print("Gunning-Fox index for {} is {}".format(file, gf.score))
+
+sys.exit(exit_code)
