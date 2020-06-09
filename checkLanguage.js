d7a9d80611266e289e9e64fd848c703f62d6c411
Ole Petter <ole.orhagen@northern.tech>
2020-06-09 19:43:55 +0200

    Replace checkLanguage with fuller implemenation
    Changelog: None
    
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- checkLanguage.js
+++ checkLanguage.js
@@ -1,17 +1,52 @@
-var retext = require('retext');
-var engine = require('unified-engine');
+var vfile = require('to-vfile')
+var report = require('vfile-reporter')
+var unified = require('unified')
+var parse = require('remark-parse')
+var stringify = require('remark-stringify')
+var remark2retext = require('remark-retext')
+var english = require('retext-english')
+var equality = require('retext-equality')
 
-engine(
-  {
-    // Standard engine configuration
-    processor: retext,
-    files: ['.'],
-    extensions: ['md', 'markdown', 'mkd', 'mkdn', 'mkdown'],
-    pluginPrefix: 'retext',
-    packageField: 'retextConfig',
-    color: true
-  },
-  (error, failed) => {
-    if (error || failed === 1) throw error;
-  }
-);
+// Spelling
+var retext = require('retext')
+var spell = require('retext-spell')
+var dictionary = require('dictionary-en')
+
+// Readability
+var readability = require('retext-readability')
+
+// Passive voice
+var passive = require('retext-passive')
+
+// a vs an
+var indefiniteArticle = require('retext-indefinite-article')
+
+var path = require('path');
+var walk = require('walk');
+var pathToFiles = '..';
+var options = {
+    followLinks: false
+};
+var walker = walk.walk(pathToFiles, options);
+walker.on('file', (root, fileStats, next) => {
+    if (!fileStats.name.endsWith('docs.md')) {
+        return next();
+    }
+    return unified()
+        .use(parse)
+        .use(
+            remark2retext,
+            unified()
+                .use(english)
+                .use(equality)
+                .use(passive)
+                .use(indefiniteArticle)
+                .use(spell, dictionary)
+                .use(readability, {age: 18})
+        )
+        .use(stringify)
+        .process(vfile.readSync(path.join(root, fileStats.name)), function(err, file) {
+            console.error(report(err || file))
+            next();
+        });
+});
