a6da5af2203e55bb47e48afb2d94dad98a4aa289
Kristian Amlie <kristian.amlie@northern.tech>
2021-07-19 10:38:31 +0200

    Move test running into a script in order to reuse from other CI jobs.
    Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>

---
+++ run-tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ex
+
+./test_autoversion.py
+./autoversion.py --check
+env TEST_OPEN_SOURCE=1 ./test_docs.py 07.Server-installation/03.Installation-with-docker-compose/docs.md
+if [ -n "$REGISTRY_MENDER_IO_PASSWORD" ]; then
+    docker login -u ntadm_menderci -p "$REGISTRY_MENDER_IO_PASSWORD" registry.mender.io
+    env TEST_ENTERPRISE=1 ./test_docs.py 07.Server-installation/03.Installation-with-docker-compose/docs.md
+    env TEST_ENTERPRISE=1 ./test_docs.py 07.Server-installation/03.Installation-with-docker-compose/02.Upgrading-from-OS-to-Enterprise/docs.md
+    env TEST_ENTERPRISE=1 ./test_docs.py 08.Server-integration/03.Mutual-TLS-authentication/docs.md
+fi
