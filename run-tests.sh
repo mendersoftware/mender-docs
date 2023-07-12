64c76be18ef97c88d9e5372bc2d41f2dc8c2ff73
Alan <alan.martinovic@northern.tech>
2023-07-12 13:59:42 +0200

    feat: remove docker-compose tests
    Ticket: None
    Changelog: None
    
    Signed-off-by: Alan <alan.martinovic@northern.tech>

--- run-tests.sh
+++ run-tests.sh
@@ -5,11 +5,8 @@ set -ex
 ./test_autoversion.py
 ./autoversion.py --check
 ./test_docs.py 03.Client-installation/02.Install-with-Debian-package/docs.md
-env TEST_OPEN_SOURCE=1 ./test_docs.py 07.Server-installation/03.Installation-with-docker-compose/docs.md
 if [ -n "$REGISTRY_MENDER_IO_PASSWORD" ]; then
     docker login -u ntadm_menderci -p "$REGISTRY_MENDER_IO_PASSWORD" registry.mender.io
-    env TEST_ENTERPRISE=1 ./test_docs.py 07.Server-installation/03.Installation-with-docker-compose/docs.md
-    env TEST_ENTERPRISE=1 ./test_docs.py 07.Server-installation/03.Installation-with-docker-compose/02.Upgrading-from-OS-to-Enterprise/docs.md
     env TEST_ENTERPRISE=1 ./test_docs.py 08.Server-integration/03.Mutual-TLS-authentication/docs.md
     env TEST_ENTERPRISE=1 ./test_docs.py 10.Downloads/docs.md
 fi
