8d7e322d60d159b694dfa4c43dbf83e42bf94dea
Daniel Skinstad Drabitzius <daniel.drabitzius@northern.tech>
2025-10-02 09:50:23 +0200

    test: update tests after bumping chapters
    We added a new chapter, 07.Orchestrate-updates, in
    faa9eee9c37d9c5019d27accd6a3e7de4f7a69e5 which resulted in the
    chapters after needing to be increases by one
    
    Signed-off-by: Daniel Skinstad Drabitzius <daniel.drabitzius@northern.tech>

--- run-tests.sh
+++ run-tests.sh
@@ -7,6 +7,6 @@ set -ex
 ./test_docs.py 03.Client-installation/02.Install-with-Debian-package/docs.md
 if [ -n "$REGISTRY_MENDER_IO_PASSWORD" ]; then
     docker login -u ntadm_menderci -p "$REGISTRY_MENDER_IO_PASSWORD" registry.mender.io
-    env TEST_ENTERPRISE=1 ./test_docs.py 09.Server-integration/04.Mender-Gateway/10.Mutual-TLS-authentication/docs.md
-    env TEST_ENTERPRISE=1 ./test_docs.py 11.Downloads/docs.md
+    env TEST_ENTERPRISE=1 ./test_docs.py 10.Server-integration/04.Mender-Gateway/10.Mutual-TLS-authentication/docs.md
+    env TEST_ENTERPRISE=1 ./test_docs.py 12.Downloads/docs.md
 fi
