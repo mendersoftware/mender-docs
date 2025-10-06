350ff07602eff09f21fc783c85f0e92e095aef09
Paweł Poławski <pawel.polawski@northern.tech>
2025-10-06 11:47:23 +0200

    ci: Update test path
    Recently Download page has been split into 3 separated chapters.
    This adds those chapters to the tested paths.
    
    Signed-off-by: Paweł Poławski <pawel.polawski@northern.tech>

--- run-tests.sh
+++ run-tests.sh
@@ -8,5 +8,7 @@ set -ex
 if [ -n "$REGISTRY_MENDER_IO_PASSWORD" ]; then
     docker login -u ntadm_menderci -p "$REGISTRY_MENDER_IO_PASSWORD" registry.mender.io
     env TEST_ENTERPRISE=1 ./test_docs.py 10.Server-integration/04.Mender-Gateway/10.Mutual-TLS-authentication/docs.md
-    env TEST_ENTERPRISE=1 ./test_docs.py 12.Downloads/docs.md
+    env TEST_ENTERPRISE=1 ./test_docs.py 12.Downloads/00.Disk-images/docs.md
+    env TEST_ENTERPRISE=1 ./test_docs.py 12.Downloads/01.Workstation-tools/docs.md
+    env TEST_ENTERPRISE=1 ./test_docs.py 12.Downloads/02.Device-components/docs.md
 fi
